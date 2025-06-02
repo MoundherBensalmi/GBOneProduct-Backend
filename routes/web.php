a8c604bbb90dbe69ac6ded6fad5cd32b36e10cad
MoundherBensalmi <moundher.bensalmi@univ-constantine2.dz>
2025-06-02 19:13:16 +0100

    - ping route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/ping', function () {
+    return response()->json([
+        'status' => 'ok',
+        'timestamp' => now(),
+    ]);
+});
