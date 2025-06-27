060a11207b28e5cfbe05ccf27f94c443253e320b
MoundherBensalmi <moundher.bensalmi@univ-constantine2.dz>
2025-06-27 07:53:36 +0100

    move ping to api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 include __DIR__ . '/api/auth.php';
 include __DIR__ . '/api/workshops.php';
 include __DIR__ . '/api/administration.php';
+
+Route::get('/ping', function () {
+    return response()->json([
+        'status' => 'ok',
+    ]);
+});
